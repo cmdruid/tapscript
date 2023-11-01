44beb42bfa604e1c6e67adbcf4356e67c5000335
cmd <cmd@yggdrasil.onion>
2023-11-01 02:34:13 -0500

    update

--- test/src/addr/p2pkh.test.ts
+++ test/src/addr/p2pkh.test.ts
@@ -8,25 +8,26 @@ import {
 
 export default function (t : Test) : void {
 
-  const ref_pubkey  = '037191e9be308354c79d9e0d596e74fce4a98768459a846a073799ad20b4c78770'
-  const ref_address = 'msi862KMaLR3jHcdKtAh9QMN2sS8Qcyywy'
-  const ref_hash    = '85be4269276fd45d0b6f7ee963dd073b202d49ed'
-  const ref_script  = [ 'OP_DUP', 'OP_HASH160', ref_hash, 'OP_EQUALVERIFY', 'OP_CHECKSIG' ]
-  const ref_object  = { type: 'p2pkh', network: 'testnet', key: ref_hash, asm: ref_script }
+  const pubkey  = '037191e9be308354c79d9e0d596e74fce4a98768459a846a073799ad20b4c78770'
+  const addr = 'msi862KMaLR3jHcdKtAh9QMN2sS8Qcyywy'
+  const key  = '85be4269276fd45d0b6f7ee963dd073b202d49ed'
+  const asm  = [ 'OP_DUP', 'OP_HASH160', key, 'OP_EQUALVERIFY', 'OP_CHECKSIG' ]
+  const hex  = `76a914${key}88ac`
+  const ref  = { asm, hex, key, network: 'testnet', type: 'p2pkh' }
 
   t.test('P2PKH unit test', t => {
     t.plan(4)
 
-    const addr1 = P2PKH.create(ref_pubkey, 'regtest')
-    t.equal(addr1, ref_address, 'Pubkey should encode into proper address.')
+    const addr1 = P2PKH.create(pubkey, 'regtest')
+    t.equal(addr1, addr, 'Pubkey should encode into proper address.')
 
-    const addr2 = P2PKH.encode(ref_hash, 'regtest')
-    t.equal(addr2, ref_address, 'Hash should encode into proper address')
+    const addr2 = P2PKH.encode(key, 'regtest')
+    t.equal(addr2, addr, 'Hash should encode into proper address')
 
-    const data  = P2PKH.decode(ref_address)
-    t.deepEqual(data, ref_object, 'Address should produce proper AddressData')
+    const data  = P2PKH.decode(addr)
+    t.deepEqual(data, ref, 'Address should produce proper AddressData')
 
-    const addr3 = create_addr(ref_script, 'regtest')
-    t.equal(addr3, ref_address, 'script should produce proper address.')
+    const addr3 = create_addr(asm, 'regtest')
+    t.equal(addr3, addr, 'script should produce proper address.')
   })
 }
--- test/src/addr/p2sh.test.ts
+++ test/src/addr/p2sh.test.ts
@@ -8,25 +8,26 @@ import {
 
 export default function (t : Test) : void {
 
-  const ref_preimg  = '001494d325b4767d23020cec68a9ca75b8fe9264b7af'
-  const ref_address = '2NFbT9Fkp7yjp22dvu7tHgikd8Yfy87KnTc'
-  const ref_hash    = 'f52611446bdfa1f67da1fb7805dbee74c6d92a54'
-  const ref_script  = [ 'OP_HASH160', ref_hash, 'OP_EQUAL' ]
-  const ref_object  = { type: 'p2sh', network: 'testnet', key: ref_hash , asm: ref_script }
+  const img  = '001494d325b4767d23020cec68a9ca75b8fe9264b7af'
+  const addr = '2NFbT9Fkp7yjp22dvu7tHgikd8Yfy87KnTc'
+  const key  = 'f52611446bdfa1f67da1fb7805dbee74c6d92a54'
+  const asm  = [ 'OP_HASH160', key, 'OP_EQUAL' ]
+  const hex  = `a914${key}87`
+  const ref  = { asm, hex, key, network: 'testnet', type: 'p2sh' }
 
   t.test('P2SH unit test', t => {
     t.plan(4)
 
-    const addr1 = P2SH.create(ref_preimg, 'regtest')
-    t.equal(addr1, ref_address, 'Script should encode into proper address.')
+    const addr1 = P2SH.create(img, 'regtest')
+    t.equal(addr1, addr, 'Script should encode into proper address.')
 
-    const addr2 = P2SH.encode(ref_hash, 'regtest')
-    t.equal(addr2, ref_address, 'Hash should encode into proper address')
+    const addr2 = P2SH.encode(key, 'regtest')
+    t.equal(addr2, addr, 'Hash should encode into proper address')
 
-    const data = P2SH.decode(ref_address)
-    t.deepEqual(data, ref_object, 'Address should produce proper AddressData')
+    const data = P2SH.decode(addr)
+    t.deepEqual(data, ref, 'Address should produce proper AddressData')
 
-    const addr3 = create_addr(ref_script, 'regtest')
-    t.equal(addr3, ref_address, 'scriptPubKey should produce proper address.')
+    const addr3 = create_addr(asm, 'regtest')
+    t.equal(addr3, addr, 'scriptPubKey should produce proper address.')
   })
 }
